8f856bdf506b3e96a8df1cb65e9cfe9f51946618
Harold Hidalgo <hahc21@gmail.com>
2024-03-11 13:27:53 -0500

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
-});
-
-module.exports = defineConfig({
   devServer: {
     disableHostCheck: true,
-  },
+  }
 });
